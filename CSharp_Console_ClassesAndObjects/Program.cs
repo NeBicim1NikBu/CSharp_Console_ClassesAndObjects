f46b4d1ba445828e15e25537d5fa9844b1afd41d
Ali Taylan <alityln@gmail.com>
2020-09-06 21:58:25 +0300

    writing to console is simplified with "WriteResults" function

--- CSharp_Console_ClassesAndObjects/Program.cs
+++ CSharp_Console_ClassesAndObjects/Program.cs
@@ -4,23 +4,32 @@ namespace CSharp_Console_ClassesAndObjects
 {
     class Program
     {
+        enum ShapeType
+        {
+            Rectangle,
+            Triangle
+        }
         static void Main(string[] args)
         {
             // Creating new oobjects from their classes with their own properities ( such as length or width )
-            RectangleClass MyRectangle = new RectangleClass(4,5);
+            RectangleClass MyRectangle = new RectangleClass(4, 5);
             TriangleClass MyRightTriangle = new TriangleClass(3, 4);
             // Using the methods within the classes
-            double AreaRect= MyRectangle.Area();
+            double AreaRect = MyRectangle.Area();
             double PerimeterRect = MyRectangle.Perimeter();
-            // Writing the results
-            Console.WriteLine($"Area of ({MyRectangle.Width} x {MyRectangle.Height}) Rectangle\t\t: {AreaRect}\n" +
-                $"Perimeter of ({MyRectangle.Width} x {MyRectangle.Height}) Rectangle\t\t: {PerimeterRect}");
-            Console.WriteLine("--------------------------------------------------");
             double AreaTriangle = MyRightTriangle.Area();
             double PerimeterTriangle = MyRightTriangle.Perimeter();
-            Console.WriteLine($"Area of ({MyRightTriangle.Width} x {MyRightTriangle.Height}) RightTriangle\t\t: {AreaTriangle}\n" +
-                $"Perimeter of ({MyRightTriangle.Width} x {MyRightTriangle.Height}) RightTriangle\t: {PerimeterTriangle}");
+            // Writing the results
+            WriteResults(ShapeType.Rectangle.ToString(), MyRectangle.Width,MyRectangle.Height,AreaRect,PerimeterRect);
+            WriteResults(ShapeType.Triangle.ToString(), MyRightTriangle.Width, MyRightTriangle.Height, AreaTriangle, PerimeterTriangle);
         }
 
+        private static void WriteResults(string ShapeType, double Width, double Height,  double Area, double Perimeter)
+        {
+            Console.WriteLine($"---------------{ShapeType}---------------------");
+            Console.WriteLine($"Area of ({Width} x {Height}) {ShapeType}\t\t: {Area}\n" +
+                $"Perimeter of ({Width} x {Height}) {ShapeType}\t\t: {Perimeter}");
+            Console.WriteLine("--------------------------------------------");
+        }
     }
 }
