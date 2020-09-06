bdfa098dc6cdd160d6154ea757d84ddcb829a775
Ali Taylan <alityln@gmail.com>
2020-09-06 16:33:34 +0300

    Add project files.

---
+++ CSharp_Console_ClassesAndObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Console_ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
